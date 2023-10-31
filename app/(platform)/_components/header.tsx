8c36c9fe78600222f27a37ad51a89b823457ca60
Nat Weerakul <pangpondwee@email.com>
2023-10-31 14:37:08 +0700

    Fix sticky nav

--- app/(platform)/_components/header.tsx
+++ app/(platform)/_components/header.tsx
@@ -2,8 +2,10 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 flex w-full justify-between items-center p-6 border-b bg-background/95 backdrop-blur-lg">
-      <h1 className="text-2xl font-semibold">Welcome back, Olivia Rhye</h1>
+    <header className="flex w-full justify-between items-center p-6 border-b">
+      <h1 className="text-xl sm:text-2xl font-semibold">
+        Welcome back, Olivia Rhye
+      </h1>
       <ModeToggle />
     </header>
   );
--- app/(platform)/_components/navbar.tsx
+++ app/(platform)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <div className="flex p-3 gap-4 border-b xl:hidden">
+    <div className="sticky top-0 z-50 flex p-3 gap-4 border-b xl:hidden  bg-background/95 backdrop-blur-lg">
       <HamburgerBar />
       <Image src={"/logo.svg"} width={142} height={32} alt="logo" />
     </div>
