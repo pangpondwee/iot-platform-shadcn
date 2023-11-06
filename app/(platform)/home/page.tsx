6d582575da483a22850718bcac2e364d411dfac8
Nat Weerakul <pangpondwee@email.com>
2023-11-06 18:40:58 +0700

    Adjust wait time for mockapi

--- app/(platform)/home/page.tsx
+++ app/(platform)/home/page.tsx
@@ -3,30 +3,7 @@ import { SearchInput } from "@/components/ui/input";
 import { SortDropDown } from "./_components/sort-dropdown";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import ProjectCard from "./_components/project-card";
-import { fetchProject } from "@/app/_api/fetchProject";
-
-function mockApi() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([
-        {
-          _id: "654505149fdd5b233c3c35c2",
-          name: "Boat track 2",
-          owner: "Withawat T",
-          location: "Bangkok",
-          imgUrl: "sadaddasdasdliscliisclasCASFsPAOfhas",
-        },
-        {
-          _id: "6547495608e26a89b5a62f33",
-          name: "Boat track 1",
-          owner: "Anan",
-          location: "Songkhla",
-          imgUrl: "sadaddasdasdliscliisclasCAS22FsPAOfhas",
-        },
-      ]);
-    }, 5000);
-  });
-}
+import { fetchProject, mockApi } from "@/app/_api/fetchProject";
 
 type project = {
   _id: string;
--- app/_api/fetchProject.tsx
+++ app/_api/fetchProject.tsx
@@ -1,7 +1,30 @@
 import axios from "axios";
 
+export const mockApi = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          _id: "654505149fdd5b233c3c35c2",
+          name: "Boat track 2",
+          owner: "Withawat T",
+          location: "Bangkok",
+          imgUrl: "sadaddasdasdliscliisclasCASFsPAOfhas",
+        },
+        {
+          _id: "6547495608e26a89b5a62f33",
+          name: "Boat track 1",
+          owner: "Anan",
+          location: "Songkhla",
+          imgUrl: "sadaddasdasdliscliisclasCAS22FsPAOfhas",
+        },
+      ]);
+    }, 2000);
+  });
+};
+
 export const axiosClient = axios.create({
-  baseURL: "http://3.27.239.197",
+  baseURL: "http://54.206.89.205",
 });
 
 export const fetchProject = async () => {
