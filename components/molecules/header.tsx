ffa681688a2bc154d66248fdd90beb3696eefc32
Nat Weerakul <pangpondwee@email.com>
2023-11-08 00:26:38 +0700

    Header can accept className

--- components/molecules/header.tsx
+++ components/molecules/header.tsx
@@ -1,29 +1,42 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface HeaderProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function Header({ children }: HeaderProps) {
+export default function Header({ children, className }: HeaderProps) {
   return (
-    <header className="flex w-full justify-between border-b p-4 sm:p-6">
+    <header
+      className={cn(
+        "flex w-full justify-between border-b p-4 sm:p-6",
+        className,
+      )}
+    >
       {children}
     </header>
   );
 }
 
-export function HeaderContent({ children }: HeaderProps) {
-  return <div className="flex flex-col gap-2">{children}</div>;
+export function HeaderContent({ children, className }: HeaderProps) {
+  return <div className={cn("flex flex-col gap-2", className)}>{children}</div>;
 }
 
-export function HeaderTitle({ children }: HeaderProps) {
-  return <h1 className="text-2xl font-semibold">{children}</h1>;
+export function HeaderTitle({ children, className }: HeaderProps) {
+  return (
+    <h1 className={cn("text-2xl font-semibold", className)}>{children}</h1>
+  );
 }
 
-export function HeaderDescription({ children }: HeaderProps) {
-  return <p className="text-base text-muted-foreground">{children}</p>;
+export function HeaderDescription({ children, className }: HeaderProps) {
+  return (
+    <p className={cn("text-base text-muted-foreground", className)}>
+      {children}
+    </p>
+  );
 }
 
-export function HeaderActions({ children }: HeaderProps) {
-  return <div className="flex gap-2">{children}</div>;
+export function HeaderActions({ children, className }: HeaderProps) {
+  return <div className={cn("flex gap-2", className)}>{children}</div>;
 }
